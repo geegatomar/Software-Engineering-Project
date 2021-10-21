06bddb373c3d8f0c4116539eb184a77369cd3cd1
Aditya Santhosh <aditya@Adityas-MacBook-Air.local>
2021-10-21 13:25:25 +0530

    Added Calendar Feature

--- email-send.js
+++ email-send.js
@@ -1,8 +1,6 @@
-const nodemailer = require("nodemailer");
+/*const nodemailer = require("nodemailer");
 
-
-
-module.exports = function (toEmail, text, subject) {
+/*module.exports = function (toEmail, text, subject) {
     const mailOptions = {
         from: "interviewbud.nitk.se.project@gmail.com",
         to: toEmail,
@@ -25,6 +23,62 @@ module.exports = function (toEmail, text, subject) {
             console.log(err);
         } else {
             console.log("Email sent: ", info.response);
+        }
+    })
+
+    
+}*/
+
+
+//To send Email with Google Calendar invite
+var nodemailer = require("nodemailer");
+const ical = require('ical-generator');
+
+module.exports = function (toEmail, text, subject, dateObj,org){
+    
+    var mailOptions = {
+        from: "interviewbud.nitk.se.project@gmail.com",
+        to: toEmail,
+        subject: subject,
+        text: text
+    }
+
+    const cal = ical({ domain: "mytestwebsite.com", name: 'My test calendar event' });
+    cal.createEvent({
+        start: dateObj,        
+        end: dateObj,            
+        summary: "INTERVIEW WITH " + org,       
+        description: "Please be ready 5 minutes before your slot", 
+        organizer: {              
+            name: org, //"MyCompany",
+            email: "interviewbud.nitk.se.project@gmail.com"
+        },
+    });
+    if (cal) {
+        let alternatives = {
+            "Content-Type": "text/calendar",
+            "method": "REQUEST",
+            "content": new Buffer(cal.toString()),
+            "component": "VEVENT",
+            "Content-Class": "urn:content-classes:calendarmessage"
+        }
+mailOptions['alternatives'] = alternatives;
+mailOptions['alternatives']['contentType'] = 'text/calendar'
+mailOptions['alternatives']['content'] 
+    = new Buffer(cal.toString())
+}
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "interviewbud.nitk.se.project@gmail.com",
+        pass: "thatsmystrongpassword"
+    }
+ });
+smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Message sent: " , response);
         }
     })
 }
